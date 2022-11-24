40a6d27499402bbcf44b81662cf4a311ec2de7f6
Jenesix <98701117+Jenesix@users.noreply.github.com>
2022-11-24 21:25:14 +0700

    WOW

---
+++ css112 lab12/Prob1_test.py
@@ -0,0 +1,8 @@
+import Prob1 as p1
+
+
+def test_1():
+    manans = "fight with Fist Hit Power is 3.0 "
+    swordmanans = "fight with Sword Hit Power is 213 "
+    p1ans = p1.Problem1()
+    assert p1ans[0] == manans and p1ans[1] == swordmanans
